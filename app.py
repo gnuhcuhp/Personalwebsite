a726eaea1717c66c2922e2465689ab00c6277886
gnuhcuhp <58369731+gnuhcuhp@users.noreply.github.com>
2025-02-11 15:55:33 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/research')
+def research():
+    return render_template('research.html')
+
+@app.route('/teaching')
+def teaching():
+    return render_template('teaching.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
